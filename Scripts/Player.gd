090f0bd97e11ef0b4dac3fdafc70c18609361855
menip <https://github.com/menip>
2017-12-14 15:28:37 -0800

    Created and setup Player scene

---
+++ Scripts/Player.gd
@@ -0,0 +1,33 @@
+extends Area2D
+
+export(int) var SPEED = 10
+export(int) var POSITION_SNAP_AMOUNT = 5
+
+# Player only interacts with objects of same color
+enum Colors {WHITE, RED, BLUE}
+export(Colors) var color = Colors.WHITE
+
+var direction
+var target_position
+var current_square
+
+func set_color(color):
+	match color:
+		WHITE:	$Sprite.modulate = Color(1,1,1)
+		RED:	$Sprite.modulate = Color(1,0,0)
+		BLUE:	$Sprite.modulate = Color(0,0,1)
+
+func _ready():
+	pass
+
+func _process(delta):
+	position += direction * SPEED * delta
+	
+	# If we are close to desired position, snap to it, and obtain further instructions
+	if (position - target_position).length() <= POSITION_SNAP_AMOUNT:
+		position = target_position
+		current_square.update_instructions()
+
+
+func _on_Player_area_entered( area ):
+	current_square = area
