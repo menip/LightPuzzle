48ae248f2259acbe8852bfebb8151d16d597d4f5
menip <https://github.com/menip>
2017-12-19 17:21:58 -0800

    Simplified Cell class, can now call update_self to update resources. Updated Global to have Modifier enum, added cell and grid size constants as well.

--- Scripts/Cell.gd
+++ Scripts/Cell.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
-enum Modifier {ANGLE, MIRROR, COLOR_SWITCH, TELLEPORT, NULL}
 export (preload("res://Scripts/Global.gd").Colors) var color = preload("res://Scripts/Global.gd").Colors.WHITE
-export (Modifier) var modifier
+export (preload("res://Scripts/Global.gd").Modifier) var modifier = preload("res://Scripts/Global.gd").Modifier.DEFAULT
 export(Vector2) var teleport_location
 export(bool) var flipped = false
 export(bool) var final = false #TODO: Implement this logic
@@ -11,19 +10,11 @@ func _ready():
 	# This is a hack and I don't like it. Seems like export (int, -1, 1, 2) doesn't work correctly: bug
 	flipped = -1 if flipped else 1
 	
-	$CellSprite.modulate = Global.which_color(color)
+	#########TODO: REMOVE##################
+	modifier = Global.Modifier.DEFAULT
+	####################################
 	
-	if modifier != Modifier.NULL:
-		$CellSprite.get_node("ModifierSprite").texture = load(match_modifier())
-		if flipped == -1:
-			$CellSprite.get_node("ModifierSprite").rotation_degrees = 90 
-
-func match_modifier():
-	match modifier:
-		ANGLE: 			return "res://Art/Angle.png"
-		MIRROR: 		return "res://Art/Mirror.png"
-		COLOR_SWITCH: 	return "res://Art/ColorSwitch.png"
-		TELLEPORT: 		return "res://Art/Telleport.png"
+	update_self()
 
 func update_instructions(player):
 	var direction = player.direction
@@ -31,19 +22,32 @@ func update_instructions(player):
 	
 	if player.color == color:
 		match modifier:
-			Modifier.ANGLE:
+			Global.Modifier.ANGLE:
 				if direction.x != 0:
 					direction = Vector2(0, direction.x * flipped)
 				elif direction.y !=0:
 					direction = Vector2(-direction.y * flipped,0)
-			Modifier.MIRROR: 
-				direction *= -1
-			Modifier.COLOR_SWITCH:
-				player.color = color 
-			Modifier.TELLEPORT:
-				 player.position = teleport_location.position
+			Global.Modifier.MIRROR: 		direction *= -1
+			Global.Modifier.COLOR_SWITCH:	player.color = color 
+			Global.Modifier.TELLEPORT:		player.position = teleport_location.position
 	
 	player.direction = direction
-	if modifier != Modifier.TELLEPORT:
+	if modifier != Global.Modifier.TELLEPORT:
 		# Set next target poisition at next cell. Multiply by 64 because that is texture size
-		player.target_position = position + direction * 64
+		player.target_position = position + direction * Global.CELL_SIZE
+
+func update_self():
+	if final: 
+		color = Global.Colors.FINAL
+
+	$CellSprite.modulate = Global.which_color(color)
+	$CellSprite.get_node("ModifierSprite").texture = load(match_modifier())
+	$CellSprite.get_node("ModifierSprite").rotation_degrees = 90 if flipped == -1 else 0
+
+func match_modifier():
+	match modifier:
+		Global.Modifier.ANGLE: 			return "res://Art/Angle.png"
+		Global.Modifier.MIRROR: 		return "res://Art/Mirror.png"
+		Global.Modifier.COLOR_SWITCH: 	return "res://Art/ColorSwitch.png"
+		Global.Modifier.TELLEPORT: 		return "res://Art/Telleport.png"
+		Global.Modifier.DEFAULT: 		return "res://Art/Cell.png"
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -1,9 +1,13 @@
 extends Node
 
-enum Colors {WHITE, RED, BLUE}
+const CELL_SIZE = Vector2(64, 64)
+const GRID_SIZE = Vector2(10, 10)
+enum Colors {WHITE, RED, BLUE, FINAL}
+enum Modifier {ANGLE, MIRROR, COLOR_SWITCH, TELLEPORT, DEFAULT}
 
 func which_color(color):
 	match color:
 		WHITE:	return Color(1,1,1)
-		RED:	return Color(1,0,0)
+		RED:	return Color(.96,.28,.28)
 		BLUE:	return Color(0,0,1)
+		FINAL: 	return Color(.28, .96, .28)
