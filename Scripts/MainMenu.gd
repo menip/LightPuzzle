911e38ae892c92573acd5d3db471f8741f9ef30d
menip <https://github.com/menip>
2018-01-04 10:13:09 -0800

    Added background and font, added polish to main menu

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,4 +4,13 @@ func _on_Start_pressed():
 	get_tree().change_scene("res://Scenes/LevelSelect.tscn")
 	
 func _on_Credits_pressed():
-	$CreditsDialog.show()
+	$Name.hide()
+	$Buttons.hide()
+	$CreditsScreen.show()
+
+
+
+func _on_CloseButton_pressed():
+	$Name.show()
+	$Buttons.show()
+	$CreditsScreen.hide()
