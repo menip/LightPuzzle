5fcb801b92afc083b20d357e7b5f9722ccf30328
menip <https://github.com/menip>
2018-01-25 22:21:44 -0800

    Moved modifier/texture map function to Global to be consistent

--- Scripts/Cell.gd
+++ Scripts/Cell.gd
@@ -9,13 +9,5 @@ func _ready():
 
 func update_self():
 	modulate = Global.which_color(color)
-	texture = load(match_modifier())
+	texture = Global.which_modifier_texture(modifier)
 	rotation_degrees = 90 if flipped == -1 else 0
-
-func match_modifier():
-	match modifier:
-		Global.Modifier.ANGLE:			return "res://Assets/Angle.png"
-		Global.Modifier.MIRROR: 		return "res://Assets/Mirror.png"
-		Global.Modifier.COLOR_SWITCH: 	return "res://Assets/ColorSwitch.png"
-		Global.Modifier.DEFAULT: 		return "" # TODO: Hack. Need to update all current levels to not have a default first, then can remove. Sends load errors atm, but no crash _/(`_`)\_
-		Global.Modifier.FINAL:			return "res://Assets/Final.png"
--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -12,9 +12,16 @@ const FINAL_LEVEL_NUMBER = 11
 var current_level = 1
 var unlocked_level = 1
 
-
 func which_color(color):
 	match color:
 		WHITE:	return Color(1,1,1)
 		RED:	return Color(.96,.28,.28)
 		BLUE:	return Color(0,0,1)
+		
+func which_modifier_texture(modifier):
+	match modifier:
+		Global.Modifier.ANGLE:			return load("res://Assets/Angle.png")
+		Global.Modifier.MIRROR: 		return load("res://Assets/Mirror.png")
+		Global.Modifier.COLOR_SWITCH: 	return load("res://Assets/ColorSwitch.png")
+		Global.Modifier.DEFAULT: 		return null # TODO: Hack. Need to update all current levels to not have a default first, then can remove. no crash _/(`_`)\_
+		Global.Modifier.FINAL:			return load("res://Assets/Final.png")
