88d94e3903b19baf2548ac0859bbb59fd9d054a0
menip <https://github.com/menip>
2018-01-25 23:05:23 -0800

    Cleaned up Game scene and script

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -3,40 +3,28 @@ extends Control
 export(PackedScene) var player
 export(PackedScene) var cell
 
-var level
+var level # TODO: Need consistency with level_number in other parts of the code
 var cells = {}
 var player_instance
 var level_area = Rect2(Vector2(), Global.CELL_SIZE * Global.GRID_SIZE)
 
-var color = Global.Colors.WHITE
-var direction_choice = Vector2(0,1)
-
 var mouse_position
-var offset = Global.CELL_SIZE/2
+
+var direction_choice = Vector2(0,1)
 
 func _ready():
 	setup_level(Global.current_level)
 	
 	$CanvasLayer/WinLossContainer.hide()
-	
-	# Setup Direction change signals
-	$CanvasLayer/HUD/Directions/Up.connect("pressed", self, "_on_direction_choice_change", [Vector2(0, -1)])
-	$CanvasLayer/HUD/Directions/Down.connect("pressed", self, "_on_direction_choice_change", [Vector2(0, 1)])
-	$CanvasLayer/HUD/Directions/Left.connect("pressed", self, "_on_direction_choice_change", [Vector2(-1, 0)])
-	$CanvasLayer/HUD/Directions/Right.connect("pressed", self, "_on_direction_choice_change", [Vector2(1, 0)])
 
 func _process(delta):
 	if player_instance != null and !level_area.has_point(player_instance.position):
 		failed()
 	
-	$Highlight.hide()
-	
+	# TODO: Need to have a check for whether game is currently active or not, so that we don't keep checking after game started
 	# Only concerned with mouse input inside of level grid
-	var snapped_position = get_global_mouse_position().snapped(Global.CELL_SIZE) #+ offset #TODO: Ask if bad practice to be creating this new var every process cycle
+	var snapped_position = get_global_mouse_position().snapped(Global.CELL_SIZE) # TODO: I should probably not be creating a new Vector2 every time
 	if level_area.has_point(snapped_position):
-		$Highlight.position = snapped_position
-		$Highlight.show()
-		
 		if Input.is_action_just_pressed("left_click") and !cells.has(snapped_position):
 			start_game(snapped_position)
 
@@ -45,6 +33,7 @@ func setup_level(level_number):
 	$CanvasLayer/HUD/LevelName.text = String(level_number)
 	load_level(level_number)
 
+# TODO: This will be greatly simplified after we switch to scenes for level storage
 func load_level(level_number):
 	var level = File.new()
 	if !level.file_exists("res://Levels/%d" % level_number):
@@ -62,7 +51,7 @@ func load_level(level_number):
 			break
 		
 		var new_cell = cell.instance()
-		new_cell.position = Vector2(current_line["posx"], current_line["posy"]) * Global.CELL_SIZE #+ offset
+		new_cell.position = Vector2(current_line["posx"], current_line["posy"]) * Global.CELL_SIZE
 		new_cell.color = int(current_line["color"])
 		new_cell.modifier = int(current_line["modifier"])
 		new_cell.flipped = current_line["flipped"]
@@ -74,11 +63,11 @@ func _on_direction_choice_change(choice):
 	direction_choice = choice
 
 func start_game(spawn_position):
-	if player_instance == null:
+	if player_instance == null: # This check needs to be done in process above
 		player_instance = player.instance()
 		player_instance.position = spawn_position 
 		player_instance.direction = direction_choice
-		player_instance.color = color
+		player_instance.color = Global.Colors.WHITE
 		
 		player_instance.target_position = spawn_position + direction_choice * Global.CELL_SIZE
 		
@@ -89,9 +78,9 @@ func start_game(spawn_position):
 
 func success():
 	# Level over, no longer need player to update
-	$Player.disconnect("update_instructions", self, "update_instructions")
+	$Player.disconnect("update_instructions", self, "update_instructions") # TODO: Kinda hackish, need to have boolean controlling game state
 	
-	# TODO: not sure why I have to cast to int
+	# Check if player unlocked a new level
 	if Global.current_level == Global.unlocked_level:
 		Global.unlocked_level += 1
 	Global.current_level += 1
@@ -112,6 +101,7 @@ func failed():
 	
 	$CanvasLayer/WinLossContainer/WinLossButton.connect("pressed", self, "reload_level")
 
+# TODO: This needs to be made better, probably moved outside of Game class
 func update_instructions():
 	var direction = player_instance.direction
 	var target_position = player_instance.target_position
@@ -147,8 +137,9 @@ func update_instructions():
 	player_instance.target_position = player_instance.position + player_instance.direction * Global.CELL_SIZE
 	
 func next_level():
+	# Load next level if there is one. If player beat last level, roll credits!
 	if int(Global.current_level) <= Global.FINAL_LEVEL_NUMBER:
-		get_tree().reload_current_scene() 
+		reload_level() 
 	else:
 		get_tree().change_scene("res://Scenes/Credits.tscn")
 	
