5d44e1f198721483d3d03cdf256257c5bb500cf9
menip <https://github.com/menip>
2018-01-28 14:18:49 -0800

    Player now updates their own instructions, instead of having Game do it.

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -12,28 +12,30 @@ var mouse_position
 
 var direction_choice = Vector2(0,1)
 
+var game_played = false
+
 func _ready():
 	setup_level(Global.current_level)
 	
 	$CanvasLayer/WinLossContainer.hide()
 
 func _process(delta):
+	# If player is alive and goes outside level bounds, game over
 	if player_instance != null and !level_area.has_point(player_instance.position):
 		failed()
 	
-	# TODO: Need to have a check for whether game is currently active or not, so that we don't keep checking after game started
-	# Only concerned with mouse input inside of level grid
-	var snapped_position = get_global_mouse_position().snapped(Global.CELL_SIZE) # TODO: I should probably not be creating a new Vector2 every time
-	if level_area.has_point(snapped_position):
-		if Input.is_action_just_pressed("left_click") and !cells.has(snapped_position):
-			start_game(snapped_position)
+	if not game_played:
+		# Only concerned with mouse input inside of level grid
+		var snapped_position = get_global_mouse_position().snapped(Global.CELL_SIZE) # TODO: I should probably not be creating a new Vector2 every time
+		if level_area.has_point(snapped_position):
+			if Input.is_action_just_pressed("left_click") and !cells.has(snapped_position):
+				start_game(snapped_position)
 
 func setup_level(level_number):
 	level = level_number
 	$CanvasLayer/HUD/LevelName.text = String(level_number)
 	load_level(level_number)
 
-# TODO: This will be greatly simplified after we switch to scenes for level storage
 func load_level(level_number):
 	var level = File.new()
 	if !level.file_exists("res://Levels/%d" % level_number):
@@ -52,7 +54,7 @@ func load_level(level_number):
 		
 		var new_cell = cell.instance()
 		new_cell.position = Vector2(current_line["posx"], current_line["posy"]) * Global.CELL_SIZE
-		new_cell.color = int(current_line["color"])
+		new_cell.color = int(current_line["color"]) # TODO: Should we use modulate values instead of a color var?
 		new_cell.modifier = int(current_line["modifier"])
 		new_cell.flipped = current_line["flipped"]
 		
@@ -63,22 +65,22 @@ func _on_direction_choice_change(choice):
 	direction_choice = choice
 
 func start_game(spawn_position):
-	if player_instance == null: # This check needs to be done in process above
-		player_instance = player.instance()
-		player_instance.position = spawn_position 
-		player_instance.direction = direction_choice
-		player_instance.color = Global.Colors.WHITE
-		
-		player_instance.target_position = spawn_position + direction_choice * Global.CELL_SIZE
-		
-		add_child(player_instance)
-		
-		# Connect player signal
-		$Player.connect("update_instructions", self, "update_instructions")
+	player_instance = player.instance()
+	player_instance.position = spawn_position 
+	player_instance.direction = direction_choice
+	player_instance.color = Global.Colors.WHITE
+	
+	player_instance.target_position = spawn_position + direction_choice * Global.CELL_SIZE
+	
+	add_child(player_instance)
+	
+	# Connect player signal
+	$Player.connect("success", self, "success")
+	$Player.connect("failed", self, "failed")
 
 func success():
-	# Level over, no longer need player to update
-	$Player.disconnect("update_instructions", self, "update_instructions") # TODO: Kinda hackish, need to have boolean controlling game state
+	game_played = true
+	remove_child(player_instance)
 	
 	# Check if player unlocked a new level
 	if Global.current_level == Global.unlocked_level:
@@ -92,8 +94,8 @@ func success():
 	$CanvasLayer/WinLossContainer/WinLossButton.connect("pressed", self, "next_level")
 
 func failed():
+	game_played = true
 	remove_child(player_instance)
-	player_instance = null
 	
 	$CanvasLayer/WinLossContainer.show()
 	$CanvasLayer/WinLossContainer/WinLossText.text = "Failed"
@@ -101,48 +103,13 @@ func failed():
 	
 	$CanvasLayer/WinLossContainer/WinLossButton.connect("pressed", self, "reload_level")
 
-# TODO: This needs to be made better, probably moved outside of Game class
-func update_instructions():
-	var direction = player_instance.direction
-	var target_position = player_instance.target_position
-	
-	# If player is in square that contains cell
-	if cells.has(player_instance.position):
-		var current_cell = cells[player_instance.position]
-		
-		# Player only interacts with objects of same color, unless we are on a color switch tile
-		if current_cell.modifier == Global.Modifier.FINAL:
-			success()
-			# NOTE: player is still on this cell, so we will continue hitting this code.
-			return
-		elif current_cell.modifier == Global.Modifier.COLOR_SWITCH:
-			player_instance.color = current_cell.color
-		elif player_instance.color == current_cell.color:
-			match current_cell.modifier:
-				Global.Modifier.ANGLE:
-					if direction.x != 0:
-						direction = Vector2(0, direction.x * -current_cell.flipped)
-					elif direction.y !=0:
-						direction = Vector2(-direction.y * current_cell.flipped,0)
-				Global.Modifier.MIRROR: 
-					# If player hits mirror at it's side, level is failed and player shatters
-					if abs(player_instance.direction.x) == current_cell.flipped or -abs(player_instance.direction.y) == current_cell.flipped: # flipped is int: 1 when not flipped, -1 when flipped
-						failed()
-						return
-					direction *= -1
-		
-		player_instance.direction = direction
-	
-	# Set next target poisition at next square.
-	player_instance.target_position = player_instance.position + player_instance.direction * Global.CELL_SIZE
-	
 func next_level():
 	# Load next level if there is one. If player beat last level, roll credits!
 	if int(Global.current_level) <= Global.FINAL_LEVEL_NUMBER:
 		reload_level() 
 	else:
 		get_tree().change_scene("res://Scenes/Credits.tscn")
-	
+
 func reload_level():
 	get_tree().reload_current_scene()
 
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,16 @@
 extends Sprite
 
-signal update_instructions
+signal success
+signal failed
 
-const SPEED = 128
-const POSITION_SNAP_AMOUNT = 5
+const SPEED = 164
+const POSITION_SNAP_AMOUNT = 2
 
 var color
 var direction
 var target_position
+
+var current_cell
 
 func _ready():
 	modulate = Global.which_color(color)
@@ -18,8 +21,40 @@ func _process(delta):
 	# If we are close to desired position, snap to it, and obtain further instructions
 	if (position - target_position).length() <= POSITION_SNAP_AMOUNT:
 		position = target_position
-		emit_signal("update_instructions")
+		update_target_position()
 		update_self()
 
 func update_self():
 	modulate = Global.which_color(color)
+
+func update_target_position():
+	# If player is in square that contains cell
+	if current_cell != null:
+		# Player only interacts with objects of same color, unless we are on a color switch tile
+		if current_cell.modifier == Global.Modifier.FINAL:
+			emit_signal("success")
+			# NOTE: player is still on this cell, so we will continue hitting this code.
+			return
+		elif current_cell.modifier == Global.Modifier.COLOR_SWITCH:
+			color = current_cell.color
+		elif color == current_cell.color:
+			match current_cell.modifier:
+				Global.Modifier.ANGLE:
+					if direction.x != 0:
+						direction = Vector2(0, direction.x * -current_cell.flipped)
+					elif direction.y !=0:
+						direction = Vector2(direction.y * -current_cell.flipped,0)
+				Global.Modifier.MIRROR: 
+					# If player hits mirror at it's side, level is failed and player shatters
+					#TODO: Set this up as either a or b equals c
+					if abs(direction.x) == current_cell.flipped or -abs(direction.y) == current_cell.flipped: # flipped is int: 1 when not flipped, -1 when flipped
+						emit_signal("failed")
+						return
+					direction *= -1
+	target_position = position + direction * Global.CELL_SIZE 
+
+func _on_Area2D_area_entered( area ):
+	current_cell = area.get_parent()
+
+func _on_Area2D_area_exited( area ):
+	current_cell = null
